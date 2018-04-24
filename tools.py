b50843666c9b3f998aa70e65854342cd86db0bb9
Guilherme Lindner <lindner.guilherme@gmail.com>
2018-04-24 11:29:32 +0200

    removed debug print

--- tools.py
+++ tools.py
@@ -167,7 +167,6 @@ def sumField(inputFile,sumField):
         
         if cellsField[0] == 999:
             result = arrayInitial + arrayVortex
-            print(result)
             outfile.write('('+str(result[0])+' '+str(result[1])+' '+str(result[2])+')\n')
             count += 1
         else:
