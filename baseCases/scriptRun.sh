281b02d45c29734b31da8a64e51d444b7927979a
Guilherme Lindner <lindner.guilherme@gmail.com>
2018-05-14 13:30:59 +0200

    fixed script folder

--- baseCases/scriptRun.sh
+++ baseCases/scriptRun.sh
@@ -1,45 +1,52 @@
 #!/bin/bash
 set -e
 
-mx=50
-my=50
-mz=25
+module load codes/openfoam/2.3.1
+
+mx=200
+my=200
+mz=100
 
 flow=0.15
 vortexR=0.01
 vortexS=50
 settleTime=0.5
-flowTime=0.5
+flowTime=0.25
 convectTime=0.5
 
 baseDir=baseTest
 baseDirVortex=baseTestVortex
+
 dir1=1_$baseDir
 dir2=2_$baseDir
 dir3=3_$baseDir
 dir4=4_$baseDir
 UTILS=~/sediUtils
+
 #################### 1 #################### 
 # 1 - create settling case dir 
 cp -r $baseDir $dir1
 cd $dir1
 
 # define domain e mesh
-sed -i "s/100 100 50/$mx $my $mz/" constant/polyMesh/blockMeshDict
+sed -i "s/mx my mz/$mx $my $mz/" constant/polyMesh/blockMeshDict
 
 # define simulation time
 sed -i "s/^endTime.*/endTime         $settleTime;/" system/controlDict
 
 # generate particles
 echo "1 - generating particles"
-python3 $UTILS/sediUtils.py -g
+python $UTILS/sediUtils.py -g
 
-# generate mesh and run simulation
+# prepare thor file
+sed -i "s/GL_1_xx/GL_1_$mx/" thor2.pbs
+
 # TODO:change blockMesh coordinates and refinment
-echo "1 - blockMesh"
+#echo "1 - blockMesh"
 ###blockMesh > log.blockMesh
-echo "1 - lammpsFoam"
+#echo "1 - lammpsFoam"
 ###lammpsFoam > log.lammpsFoam
+echo "1 - launching simulation"
 qsub thor1.pbs
 
 
@@ -47,7 +54,7 @@ qsub thor1.pbs
 while ! [ -f $settleTime/Ub ];
 do
     echo "waiting lammpsFoam to finish"
-    sleep 60
+    sleep 300
 done 
 cd ..
 
@@ -60,31 +67,34 @@ cd $dir2
 sed -i "s/^endTime.*/endTime         $flowTime;/" system/controlDict
 
 # convert settled particles into LAMMPS format 
-python3 $UTILS/sediUtils.py -c -i $settleTime/lagrangian/defaultCloud/positions
+python $UTILS/sediUtils.py -c -i $settleTime/lagrangian/defaultCloud/positions
+
 rm -rf 0.25 0.5
+rm log*
 
 # prepare thor file
 mv thor1.pbs thor2.pbs
-sed -i "s/GL_1_02/GL_2_$mx/" thor2.pbs
-sed -i "s/1_baseTest/2_baseTest" thor2.pbs
+sed -i "s/GL_1_$mx/GL_2_$mx/" thor2.pbs
+sed -i "s/1_baseTest/2_baseTest/" thor2.pbs
 
 # remove unnecessary particles
-python3 $UTILS/sediUtils.py -r -i In_OF.in
+python $UTILS/sediUtils.py -r -i In_OF.in
 
 # freeze all particles
-python3 $UTILS/sediUtils.py -f -i In_Removed.in
+python $UTILS/sediUtils.py -f -i In_Removed.in
 sed -i "s/In_initial.in/In_Frozen.in/" in.lammps
 
 # define simulation time
 sed -i "s/endTime         1;/endTime         $flowTime;/" system/controlDict
 
 # set flow velocity
-sed -i "s/.*Ubar.*/Ubar            Ubar [0 1 -1 0 0 0 0] ($flow 0 0); \/\/ average velocity in the channel/" constant/transportProperties
+sed -i "s/.*Ubar.*/Ubar            Ubar [0 1 -1 0 0 0 0] ($flow 0 0);/" constant/transportProperties
 
 # run simulation
 # TODO:change blockMesh coordinates and refinment
-echo "2 - lammpsFoam"
+#echo "2 - lammpsFoam"
 #lammpsFoam > log.lammpsFoam
+echo "2 - launching simulation"
 qsub thor2.pbs
 
 #check if the final file has been generated
@@ -102,7 +112,7 @@ cp -r $baseDirVortex $dir3
 cd $dir3
 
 # define domain and mesh
-sed -i "s/100 100 50/$mx $my $mz/" constant/polyMesh/blockMeshDict
+sed -i "s/mx my mz/$mx $my $mz/" constant/polyMesh/blockMeshDict
 
 # define vortex properties
 sed -i "s/.*speed=50;.*/            \"speed=$vortexS;\"/" system/fvOptions
@@ -125,20 +135,23 @@ cd ..
 cp -r $dir2 $dir4
 cd $dir4
 rm -rf 0.25 0.5
+rm log*
 
 # define simulation time
 sed -i "s/^endTime.*/endTime         $convectTime;/" system/controlDict
+sed -i "s/^writeInterval.*/writeInterval   0.01;/" system/controlDict
 
 # prepare thor file
 mv thor2.pbs thor4.pbs
 sed -i "s/GL_2_$mx/GL_4_$mx/" thor4.pbs
-sed -i "s/2_baseTest/4_baseTest" thor2.pbs
-echo "foamToVTK -fields '(alpha p Q Ua Ub)'" >> thor4.pbs
+sed -i "s/2_baseTest/4_baseTest/" thor4.pbs
+echo "Q" >> thor4.pbs
+echo "foamToVTK -fields '(alpha p Q Ua Ub)' -allPatches" >> thor4.pbs
 
 # generate combined velocity
 cp ../$dir3/0/UbCells .
 echo "4 - combining velocity fields"
-python3 $UTILS/sediUtils.py -s ../$dir3/1/U -i ../$dir2/$flowTime/Ub
+python $UTILS/sediUtils.py -s ../$dir3/1/U -i ../$dir2/$flowTime/Ub
 mv newUb 0/Ub
 
 
@@ -146,12 +159,13 @@ mv newUb 0/Ub
 sed -i "s/endTime         1;/endTime         $convectTime;/" system/controlDict 
 
 # activate particles
-python3 $UTILS/sediUtils.py -a -i In_Frozen.in
+python $UTILS/sediUtils.py -a -i In_Frozen.in
 sed -i "s/In_Frozen.in/In_Active.in/" in.lammps
 
 # run simulation
-echo "4 - lammpsFoam"
+#echo "4 - lammpsFoam"
 #lammpsFoam > log.lammpsFoam
-echo "4 - foamToVTK"
+#echo "4 - foamToVTK"
 #foamToVTK > log.foamToVTK
+echo "4 - launching simulation"
 qsub thor4.pbs
