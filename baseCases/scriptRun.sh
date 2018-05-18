49515bca6c717aed4dcb30777c9d02beac3d7ece
Guilherme Lindner <lindner.guilherme@gmail.com>
2018-05-18 14:06:15 +0200

    improved log output from script

--- baseCases/scriptRun.sh
+++ baseCases/scriptRun.sh
@@ -25,7 +25,6 @@ dir4=4_$baseDir
 UTILS=~/sediUtils
 
 
-<<<<<<< HEAD
 run1 () {
     # 1 - create settling case dir 
     cp -r $baseDir $dir1
@@ -56,8 +55,8 @@ run1 () {
     #check if the final file has been generated
     while ! [ -f $settleTime/Ub ];
     do
-        echo "waiting lammpsFoam to finish"
-        sleep 300
+        echo "1 - waiting lammpsFoam to finish"
+        sleep 600
     done 
     cd ..
 }
@@ -104,8 +103,8 @@ run2 () {
     #check if the final file has been generated
     while ! [ -f $flowTime/Ub ];
     do
-        echo "waiting lammpsFoam to finish"
-        sleep 60
+        echo "2 - waiting lammpsFoam to finish"
+        sleep 600
     done 
     
     cd ..
@@ -198,147 +197,4 @@ if [ -f $dir4/$convectTime/Ub ]; then
 else
     run4
 fi
-echo "done"
-=======
-# define domain e mesh
-sed -i "s/mx my mz/$mx $my $mz/" constant/polyMesh/blockMeshDict
-
-# define simulation time
-sed -i "s/^endTime.*/endTime         $settleTime;/" system/controlDict
-
-# generate particles
-echo "1 - generating particles"
-python $UTILS/sediUtils.py -g
-
-# prepare thor file
-sed -i "s/GL_1_xx/GL_1_$mx/" thor1.pbs
-
-# TODO:change blockMesh coordinates and refinment
-#echo "1 - blockMesh"
-###blockMesh > log.blockMesh
-#echo "1 - lammpsFoam"
-###lammpsFoam > log.lammpsFoam
-echo "1 - launching simulation"
-qsub thor1.pbs
-
-
-#check if the final file has been generated
-while ! [ -f $settleTime/Ub ];
-do
-    echo "waiting lammpsFoam to finish"
-    sleep 300
-done 
-cd ..
-
-#################### 2 #################### 
-# 2 - create normal flow case dir
-cp -r $dir1 $dir2
-cd $dir2
-
-# define simulation time
-sed -i "s/^endTime.*/endTime         $flowTime;/" system/controlDict
-
-# convert settled particles into LAMMPS format 
-python $UTILS/sediUtils.py -c -i $settleTime/lagrangian/defaultCloud/positions
-
-rm -rf 0.25 0.5
-rm log*
-
-# prepare thor file
-mv thor1.pbs thor2.pbs
-sed -i "s/GL_1_$mx/GL_2_$mx/" thor2.pbs
-sed -i "s/1_baseTest/2_baseTest/" thor2.pbs
-
-# remove unnecessary particles
-python $UTILS/sediUtils.py -r -i In_OF.in
-
-# freeze all particles
-python $UTILS/sediUtils.py -f -i In_Removed.in
-sed -i "s/In_initial.in/In_Frozen.in/" in.lammps
-
-# define simulation time
-sed -i "s/endTime         1;/endTime         $flowTime;/" system/controlDict
-
-# set flow velocity
-sed -i "s/.*Ubar.*/Ubar            Ubar [0 1 -1 0 0 0 0] ($flow 0 0);/" constant/transportProperties
-
-# run simulation
-# TODO:change blockMesh coordinates and refinment
-#echo "2 - lammpsFoam"
-#lammpsFoam > log.lammpsFoam
-echo "2 - launching simulation"
-qsub thor2.pbs
-
-#check if the final file has been generated
-while ! [ -f $flowTime/Ub ];
-do
-    echo "waiting lammpsFoam to finish"
-    sleep 60
-done 
-
-cd ..
-
-#################### 3 #################### 
-# 3 - create vortex case dir
-cp -r $baseDirVortex $dir3
-cd $dir3
-
-# define domain and mesh
-sed -i "s/mx my mz/$mx $my $mz/" constant/polyMesh/blockMeshDict
-
-# define vortex properties
-sed -i "s/.*speed=50;.*/            \"speed=$vortexS;\"/" system/fvOptions
-sed -i "s/.*radius=0.01;.*/            \"radius=$vortexR;\"/" system/fvOptions
-
-# generate mesh and run simulation
-# TODO:change blockMesh coordinates and refinment
-echo "3 - blockMesh"
-blockMesh > log.blockMesh
-echo "3 - simpleFoam"
-simpleFoam > log.simpleFoam
-
-
-#prepare fields
-setFields > log.setFields
-cd ..
-
-#################### 4 #################### 
-# 4 - create convective case dir
-cp -r $dir2 $dir4
-cd $dir4
-rm -rf 0.25 0.5
-rm log*
-
-# define simulation time
-sed -i "s/^endTime.*/endTime         $convectTime;/" system/controlDict
-sed -i "s/^writeInterval.*/writeInterval   0.01;/" system/controlDict
-
-# prepare thor file
-mv thor2.pbs thor4.pbs
-sed -i "s/GL_2_$mx/GL_4_$mx/" thor4.pbs
-sed -i "s/2_baseTest/4_baseTest/" thor4.pbs
-echo "Q" >> thor4.pbs
-echo "foamToVTK -fields '(alpha p Q Ua Ub)' -allPatches" >> thor4.pbs
-
-# generate combined velocity
-cp ../$dir3/0/UbCells .
-echo "4 - combining velocity fields"
-python $UTILS/sediUtils.py -s ../$dir3/1/U -i ../$dir2/$flowTime/Ub
-mv newUb 0/Ub
-
-
-# define simulation time
-sed -i "s/endTime         1;/endTime         $convectTime;/" system/controlDict 
-
-# activate particles
-python $UTILS/sediUtils.py -a -i In_Frozen.in
-sed -i "s/In_Frozen.in/In_Active.in/" in.lammps
-
-# run simulation
-#echo "4 - lammpsFoam"
-#lammpsFoam > log.lammpsFoam
-#echo "4 - foamToVTK"
-#foamToVTK > log.foamToVTK
-echo "4 - launching simulation"
-qsub thor4.pbs
->>>>>>> 9f232da026407b693541bba740f2d8c139bfb6af
+echo "script finished"
